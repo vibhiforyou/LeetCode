e4c52aaa0713a3c5d7d90e3638ffcb6722a7360a
vibhiforyou <vibhiforyou@gmail.com>
2021-09-21 23:50:14 +0530

    21-sept-2021 merge two sorted arrays

---
+++ src/leetcode/problems/medium/ThreeSumClosest/Main.java
@@ -0,0 +1,50 @@
+package leetcode.problems.medium.ThreeSumClosest;
+
+
+import com.sun.source.tree.BreakTree;
+
+import java.util.Arrays;
+
+class Solution {
+    //Logic runForLoop and kep two pointers
+    //pointer one should start after first for loop and second pointer should start from last.
+    public int threeSumClosest(int[] nums, int target) {
+        int n = nums.length;
+        int diff = Integer.MAX_VALUE;
+        int ans=0;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            int l = i + 1;
+            int r = n - 1;
+
+
+            while (l < r) {
+                int sum = nums[i]+nums[l]+nums[r];
+                if(sum==target){
+                    return nums[i]+nums[l]+nums[r];
+                }
+                if(sum<target){
+                    l++;
+                }
+                else{
+                    r--;
+                }
+                int localDiff=Math.abs(sum-target);
+
+                if(localDiff<diff){
+                    diff=localDiff;
+                    ans=sum;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+public class Main {
+    public static void main(String... args) {
+        Solution solution = new Solution();
+        System.out.println(solution.threeSumClosest(new int[]{1,1,-1,-1,3}, -1));
+    }
+}
