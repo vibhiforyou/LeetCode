f75d892a8f11d2af5d5f01b2d7ccf1a0f5ea1c5d
vibhiforyou <vibhiforyou@gmail.com>
2021-10-20 23:18:22 +0530

    power of three

---
+++ src/leetcode/problems/medium/FourSum/Main.java
@@ -0,0 +1,21 @@
+package leetcode.problems.medium.FourSum;
+
+
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+
+        return null;
+    }
+
+
+}
+
+public class Main {
+    public static void main(String... args) {
+        Solution solution = new Solution();
+        solution.fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0);
+    }
+}
